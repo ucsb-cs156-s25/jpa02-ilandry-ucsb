73c800ad8d7227ef80ffbf9ee25f4d676d93dbf3
Isaac Landry <isaac@cinderace.attlocal.net>
2025-04-04 21:38:31 -0700

    Added a test case for team.equals().

--- src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
@@ -23,4 +23,22 @@ public class TeamTest {
     // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
     // 100% mutation coverage (all mutants timed out or killed)
 
+    @Test
+    public void toString_returns_correct_string() {
+        assertEquals("Team(name=test-team, members=[])", team.toString());
+    }
+
+    @Test
+    public void  equals_is_correct() {
+
+        assertEquals(team.equals(team), true);
+        String test_team_name = "s";
+        assertEquals(team.equals(test_team_name), false);        
+
+        Team test_team_2 = new Team("test-team");
+        assertEquals(team.equals(test_team_2), true);
+        
+        test_team_2.addMember("0");
+        assertEquals(team.equals(test_team_2), false);
+    }
 }
